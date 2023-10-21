b58ffa9d15aa815aecc1ddf81d09d2e314c12c32
Nicolás Torres <nicolastorres250@gmail.com>
2023-10-21 19:02:19 -0300

    add: alta productos backend

---
+++ Codigo/Backend/PharmaGo.WebApi/Models/In/ProductModel.cs
@@ -0,0 +1,22 @@
+﻿using PharmaGo.Domain.Entities;
+
+namespace PharmaGo.WebApi.Models.In
+{
+    public class ProductModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PharmacyName { get; set; }
+
+        public Product ToEntity() => new()
+        {
+            Code = Code,
+            Name = Name,
+            Description = Description,
+            Price = Price,
+            Pharmacy = new Pharmacy() { Name = PharmacyName }
+        };
+    }
+}
